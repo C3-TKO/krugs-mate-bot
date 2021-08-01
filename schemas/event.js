396f9f36bbb5771ece36e179599b5394d286c4a2
Thomas Kolar 🐻 <thomas.kolar@gmail.com>
2021-08-01 15:09:11 +0200

    feat: add first commands and setup db connection

---
+++ schemas/event.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const eventSchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    required: [true, 'Date of event is required']
+  },
+  created: {
+    type: Date,
+    required: [true, 'Created date is required']
+  }
+})
+
+module.exports = eventSchema
